da95eee2f78fdad530f63ce6c50de813ed9fd376
Cheung Kin Wong <Cheungkin.wong@gmail.com>
2019-05-29 14:41:39 +0200

    remove old code

--- assets/js/nyancat.js
+++ assets/js/nyancat.js
@@ -1,35 +1,8 @@
-// const onClick = () => {
-//   const cat = document.getElementById("nyan");
-//   const song = document.getElementById("nyansong");
-//   const button = document.getElementById("nyancat");
-
-//   song.play();
-//   document.body.style.background = "url('./assets/img/nyanbackground.png')";
-//   button.style.display = "none";
-
-//   const frame = () => {
-//     if (pos == 620) {
-//       clearInterval(id);
-//     } else {
-//       pos++;
-//       cat.style.top = pos + "px";
-//       cat.style.left = pos + "px";
-//     }
-//   };
-
-//   let id = setInterval(frame, 5);
-//   let pos = -100;
-//   setInterval(onClick, 4000);
-// };
-
-//new try
-
 const onClick = () => {
   const cat = document.getElementById("nyan");
   const song = document.getElementById("nyansong");
   const button = document.getElementById("nyancat");
   var seconds = 0;
-  let timer = seconds;
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
